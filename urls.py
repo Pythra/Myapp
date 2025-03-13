61f987dd6965a9f6272167164a76aa53c580200b
unknown <peze336@gmail.com>
2025-03-13 03:01:20 -0700

    progress

--- urls.py
+++ urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import PasswordResetRequestView, PasswordResetConfirmView
 
 urlpatterns = [
     path('api/password-reset/', PasswordResetRequestView.as_view(), name='password_reset_request'),
     path('api/password-reset-confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
