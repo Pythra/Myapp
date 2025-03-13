9c1bd419b9fb16c003d077a16cd6ddee5794383d
unknown <peze336@gmail.com>
2025-03-13 02:29:32 -0700

    progress

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('asap.urls')),  # Updated to namespace 'api'
+    path('api/', include('asap.urls')),
+    path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html', extra_context={'next': '/login/'}), name='password_change_done'),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
